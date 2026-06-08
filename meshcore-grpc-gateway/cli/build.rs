47eb8e9ee1748696c7bb1563554e4be791e01f27
Kevin Lutzer <kevinlutzer@proton.me>
2026-06-07 23:43:12 -0400

    fix issues with snap builds

--- meshcore-grpc-gateway/cli/build.rs
+++ meshcore-grpc-gateway/cli/build.rs
@@ -2,6 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(false)
         .build_client(true)
-        .compile_protos(&["../proto/meshcore.proto"], &["../proto"])?;
+        .compile_protos(&["../proto/meshcore.proto"], &["../proto", "/usr/include", "/usr/local/include"])?;
     Ok(())
 }
--- meshcore-grpc-gateway/server/build.rs
+++ meshcore-grpc-gateway/server/build.rs
@@ -2,6 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(true)
         .build_client(false)
-        .compile_protos(&["../proto/meshcore.proto"], &["../proto"])?;
+        .compile_protos(&["../proto/meshcore.proto"], &["../proto", "/usr/include", "/usr/local/include"])?;
     Ok(())
 }
